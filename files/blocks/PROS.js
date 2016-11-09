e8bc8d841d02c66dc4c9eccd56a2ab559497c864
Moocow9m <moocow9m@gmail.com>
2016-11-08 16:21:20 -0800

    Switch from javascript blocks to JSON blocks and other misc edits

--- files/blocks/PROS.js
+++ files/blocks/PROS.js
@@ -1,67 +1,123 @@
 Blockly.Blocks['setmotorspeed'] = {
   init: function() {
-    this.appendDummyInput()
-        .appendField("Set Motor Speed:")
-        .appendField("Port:")
-        .appendField(new Blockly.FieldNumber(1, 1, 10), "Port")
-        .appendField("Speed:")
-        .appendField(new Blockly.FieldNumber(0, -127, 127), "Speed");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+	  this.jsonInit(
+	  {
+  "type": "setmotorspeed",
+  "message0": "Set Motor Speed: Port: %1 Speed: %2",
+  "args0": [
+    {
+      "type": "field_number",
+      "name": "Port",
+      "value": 1,
+      "min": 1,
+      "max": 10
+    },
+    {
+      "type": "field_number",
+      "name": "Speed",
+      "value": 0,
+      "min": -127,
+      "max": 127
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": "http://www.example.com/"
+}
+	  );
   }
 };
 
 Blockly.Blocks['initcode'] = {
   init: function() {
-    this.appendDummyInput()
-        .appendField("InitIO");
-    this.appendStatementInput("InitIOVars")
-        .setCheck(null);
-    this.appendDummyInput()
-        .appendField("Init");
-    this.appendStatementInput("InitVars")
-        .setCheck(null);
-    this.setColour(270);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+	  this.jsonInit({
+		  "type": "initcode",
+		  "message0": "InitIO %1 %2 Init %3 %4",
+		  "args0": [
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_statement",
+      "name": "InitIOVars",
+      "check": "setmotorspeed"
+    },
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_statement",
+      "name": "InitVars"
+    }
+  ],
+  "colour": 270,
+  "tooltip": "",
+  "helpUrl": "http://www.example.com/"
+	  });
   }
 };
 
 Blockly.Blocks['main_h'] = {
   init: function() {
-    this.appendDummyInput()
-        .appendField("Global variable initialization");
-    this.appendStatementInput("MainVars")
-        .setCheck(null);
-    this.setColour(65);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+	  this.jsonInit({
+		  "type": "main_h",
+  "message0": "Global variable initialization %1 %2",
+  "args0": [
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_statement",
+      "name": "MainVars"
+    }
+  ],
+  "colour": 65,
+  "tooltip": "",
+  "helpUrl": "http://www.example.com/"
+	  });
   }
 };
 
 Blockly.Blocks['autonomous'] = {
   init: function() {
-    this.appendDummyInput()
-        .appendField("Autonomous");
-    this.appendStatementInput("AutonVars")
-        .setCheck(null);
-    this.setColour(160);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+	  this.jsonInit({
+		  "type": "autonomous",
+  "message0": "Autonomous %1 %2",
+  "args0": [
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_statement",
+      "name": "AutonVars"
+    }
+  ],
+  "colour": 160,
+  "tooltip": "",
+  "helpUrl": "http://www.example.com/"
+  });
   }
 };
 
 Blockly.Blocks['opcontrol'] = {
   init: function() {
-    this.appendDummyInput()
-        .appendField("Operator Control");
-    this.appendStatementInput("NAME")
-        .setCheck(null);
-    this.setColour(260);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+	  this.jsonInit({
+		  "type": "opcontrol",
+  "message0": "Operator Control %1 %2",
+  "args0": [
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_statement",
+      "name": "NAME"
+    }
+  ],
+  "colour": 260,
+  "tooltip": "",
+  "helpUrl": "http://www.example.com/"
+  });
   }
 };
